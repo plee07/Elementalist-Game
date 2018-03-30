939178ed3f13b3af15aabaa3ebc2019cb9175187
PHIL\Phil Lee <phil.j.lee1@gmail.com>
2018-03-29 22:14:06 -0400

    Commit 3
    Updated to include mulitple enemies + wall collision

--- Elementalist/Projectile.h
+++ Elementalist/Projectile.h
@@ -6,7 +6,6 @@ class Projectile : public Entity
 {
 public:
 	float movementSpeed;
-	int attackDamage; // I don't really need this
 	int direction; // 1 - up, 2 - down, 3 - left, 4 - right
 	Texture PushProjectile;
 
